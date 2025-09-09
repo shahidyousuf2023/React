4aa6cc56e8a8604c6746612419d68839feb173ed
shahidyousuf2023 <shahidyousufgb@gmail.com>
2025-09-09 10:58:39 +0500

    initial commit

---
+++ src/User.jsx
@@ -0,0 +1,25 @@
+function Login(){
+return (
+    <dive>
+        <h1>Login</h1>
+    </dive>
+)
+}
+export function Profile(){
+    return (
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+export function Setting(){
+    return (
+        <div>
+            <h1>Setting</h1>
+        </div>
+    )
+}
+
+export default Login
+
+export const UserKey="@aaaaaaaa"
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 import App from './App.jsx'
+import Login,{Profile,Setting, UserKey} from './User.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Login />
+    <Profile />
+    <Setting />
+    <h1>{UserKey}</h1>
   </StrictMode>,
 )
